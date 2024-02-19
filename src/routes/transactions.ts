f54dd6d8d791634c60bd65222758e349e307394d
poy <ghhh7731@gmail.com>
2024-02-19 12:35:39 +0700

    ci: :memo: edit valuu insert

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -8,40 +8,41 @@ const router = Router();
 router.post(
 	"/transactions",
 	(req, _res, next) => {
-		const reqParamCustomer = z.object({
+		const reqBodyTransactions = z.object({
 			customers_id: z.number(),
 			food_item_id: z.number(),
 			quantity: z.number(),
 		});
 
-		const { success } = reqParamCustomer.safeParse(req.params);
+		const { success } = reqBodyTransactions.safeParse(req.body);
 
 		if (success) {
 			next();
 		} else {
-			next(badRequest("invalid param"));
+			next(badRequest("invalid body"));
 		}
 	},
 
 	async (req, res, next) => {
 		try {
-			const {
-				customers_id: reqBodyCustomerId,
-				food_item_id: reqBodyFoodItemId,
-				quantity: reqBodyQuantity,
+			const valueInsert: {
+				customers_id: number;
+				food_item_id: number;
+				quantity: number;
 			} = req.body;
 			const connection = await pool.getConnection();
 
 			try {
 				await connection.beginTransaction();
 				const [row] = await connection.query<ResultSetHeader>(
-					`INSERT INTO transactions (customers_id) VALUES ("${reqBodyCustomerId}");`,
+					`INSERT INTO transactions (customer_id) VALUES ("${valueInsert.customers_id}");`,
 				);
+
 				const result = await connection.query(
-					`INSERT INTO order_items (transactions_id, food_items_id, quantity) VALUES "${row.insertId}","${reqBodyFoodItemId}","${reqBodyQuantity}")`,
+					`INSERT INTO order_items (transaction.id, food_item_id, quantity) VALUES "${row.insertId}","${valueInsert.food_item_id}","${valueInsert.quantity}");`,
 				);
 				await connection.commit();
-				res.status(200).json({ result: "INSERT transactions success" });
+				res.status(200).json({ result: result.at(0) });
 			} catch (error) {
 				await connection.rollback();
 				next(error);
