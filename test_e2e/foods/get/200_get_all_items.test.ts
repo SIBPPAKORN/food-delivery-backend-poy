b6a3f56369710e0006d8df961fcd116006559d4f
poy <ghhh7731@gmail.com>
2024-02-20 12:16:14 +0700

    ci: :memo: add 200 test

--- test_e2e/foods/get/200_get_all_items.test.ts
+++ test_e2e/foods/get/200_get_all_items.test.ts
@@ -6,21 +6,39 @@ afterAll(() => {
 });
 
 test("should create fooditems success 15 item", async () => {
-	const response = await request(app)
-		.get("/food_items")
-		.expect("Content-Type", /json/)
-		.expect(200);
+	const response = await request(app).get("/foods").expect("Content-Type", /json/).expect(200);
 
 	expect(response.body.result.length).toBe(15);
 });
 
 test("should create fooditems category:Main Courses success 2 item", async () => {
 	const response = await request(app)
-		.get("/food_items")
+		.get("/foods")
 		.query({ category: "Main Courses" })
-
 		.expect("Content-Type", /json/)
 		.expect(200);
 
 	expect(response.body.result.length).toBe(2);
+});
+
+test("should create fooditems search: t Courses success 2 item", async () => {
+	const response = await request(app)
+		.get("/foods")
+		.query({ search: "t" })
+
+		.expect("Content-Type", /json/)
+		.expect(200);
+
+	expect(response.body.result.length).toBe(9);
+});
+
+test("should create fooditems search: t Courses success 2 item", async () => {
+	const response = await request(app)
+		.get("/foods")
+		.query({ name: "ASC" })
+
+		.expect("Content-Type", /json/)
+		.expect(200);
+
+	expect(response.body.result.length).toBe(15);
 });
