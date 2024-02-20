4144791fd25120ff6f4d23762ce9f7d46d56dfb6
poy <ghhh7731@gmail.com>
2024-02-20 12:07:49 +0700

    ci: :memo: add shema and typeof and add obtions

--- src/routes/foods.ts
+++ src/routes/foods.ts
@@ -8,6 +8,8 @@ const router = Router();
 
 const schema = z.object({
 	category: z.coerce.string().optional(),
+	search: z.coerce.string().optional(),
+	name: z.coerce.string().optional(),
 });
 
 router.get(
@@ -25,15 +27,34 @@ router.get(
 	async (req, res, next) => {
 		try {
 			const connection = await pool.getConnection();
+			const category = typeof req.query.category === "string" ? req.query.category : null;
+			const search = typeof req.query.search === "string" ? req.query.search : null;
+			const name = typeof req.query.name === "string" ? req.query.name : null;
+
+			function categoryConnition(category: string | null): string {
+				return category
+					? `AND food_items.category = ${mysql.escape(req.query.category)}`
+					: "";
+			}
+
+			function nameConnition(name: string | null): string {
+				return name ? `ORDER BY food_items.name ${name} ` : "ORDER BY food_items.name ASC";
+			}
+
+			function searchConnition(search: string | null): string {
+				return search ? `AND food_items.name LIKE ${mysql.escape(`%${search}%`)} ` : "";
+			}
 
 			try {
 				const sqlSelect = "SELECT *";
-				const sqlFrom = " FROM food_items";
-				const sqlWhere = req.query.category
-					? `WHERE food_items.category = ${mysql.escape(req.query.category)}`
-					: "";
-				const sqlCommand = `${sqlSelect} ${sqlFrom} ${sqlWhere}`;
+				const sqlFrom = "FROM food_items";
+				const sqlWhere = `WHERE 1=1 ${categoryConnition(category)}  ${searchConnition(
+					search,
+				)}`;
+				const sqlOrderBy = nameConnition(name);
 
+				const sqlCommand = `${sqlSelect} ${sqlFrom} ${sqlWhere} ${sqlOrderBy}`;
+				console.log(sqlCommand);
 				const [data, _metaData] = await connection.query(sqlCommand);
 				res.status(200).json({ result: data });
 			} catch (error) {
