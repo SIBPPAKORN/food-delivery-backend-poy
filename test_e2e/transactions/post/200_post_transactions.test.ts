7630acb4c853732473c47f91179d78e8eec43e64
poy <ghhh7731@gmail.com>
2024-03-01 14:27:26 +0700

    ci: :memo: add beforeAll

--- test_e2e/transactions/post/200_post_transactions.test.ts
+++ test_e2e/transactions/post/200_post_transactions.test.ts
@@ -1,6 +1,11 @@
 import { RowDataPacket } from "mysql2";
 import supertest from "supertest";
 import { app, pool } from "../../../src/app";
+import { restoreDb } from "../../utils/database";
+
+beforeAll(() => {
+	restoreDb();
+});
 
 afterAll(() => {
 	pool.end();
@@ -10,7 +15,7 @@ test("check data before create transactions", async () => {
 	const connection = await pool.getConnection();
 
 	const [data, _metaData] = await connection.query<RowDataPacket[]>("SELECT * FROM transactions");
-	expect(data).toHaveLength(1);
+	expect(data).toHaveLength(0);
 	connection.release();
 });
 
@@ -36,7 +41,7 @@ test("check data after create transactions", async () => {
 	const connection = await pool.getConnection();
 
 	const [data, _metaData] = await connection.query<RowDataPacket[]>("SELECT * FROM transactions");
-	expect(data).toHaveLength(2);
+	expect(data).toHaveLength(1);
 
 	connection.release();
 });
--- test_e2e/transactions/post/400_post_transactions.test.ts
+++ test_e2e/transactions/post/400_post_transactions.test.ts
@@ -1,6 +1,11 @@
 import { RowDataPacket } from "mysql2";
 import supertest from "supertest";
 import { app, pool } from "../../../src/app";
+import { restoreDb } from "../../utils/database";
+
+beforeAll(() => {
+	restoreDb();
+});
 
 afterAll(() => {
 	pool.end();
@@ -10,7 +15,7 @@ test("check data before create transactions", async () => {
 	const connection = await pool.getConnection();
 
 	const [data, _metaData] = await connection.query<RowDataPacket[]>("SELECT * FROM transactions");
-	expect(data).toHaveLength(2);
+	expect(data).toHaveLength(0);
 	connection.release();
 });
 
@@ -37,7 +42,7 @@ test("check data after create transactions", async () => {
 	const connection = await pool.getConnection();
 
 	const [data, _metaData] = await connection.query<RowDataPacket[]>("SELECT * FROM transactions");
-	expect(data).toHaveLength(2);
+	expect(data).toHaveLength(0);
 
 	connection.release();
 });
