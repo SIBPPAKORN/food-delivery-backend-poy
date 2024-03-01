190bdf4c71551329276e89f94bf9297534365fd4
poy <ghhh7731@gmail.com>
2024-03-01 12:24:35 +0700

    ci: :memo: rename

---
+++ test_e2e/transactions/post/200_post_transactions.test.ts
@@ -0,0 +1,42 @@
+import { RowDataPacket } from "mysql2";
+import supertest from "supertest";
+import { app, pool } from "../../../src/app";
+
+afterAll(() => {
+	pool.end();
+});
+
+test("check data before create transactions", async () => {
+	const connection = await pool.getConnection();
+
+	const [data, _metaData] = await connection.query<RowDataPacket[]>("SELECT * FROM transactions");
+	expect(data).toHaveLength(0);
+	connection.release();
+});
+
+test("create transactions", async () => {
+	const response = await supertest(app)
+		.post("/transactions")
+		.send({
+			customer_id: 1,
+			items: [
+				{
+					food_item_id: 1,
+					quantity: 1,
+				},
+			],
+		})
+		.expect("Content-Type", /json/)
+		.expect(200);
+
+	expect(response.body.result).toEqual("ok");
+});
+
+test("check data before create transactions", async () => {
+	const connection = await pool.getConnection();
+
+	const [data, _metaData] = await connection.query<RowDataPacket[]>("SELECT * FROM transactions");
+	expect(data).toHaveLength(0);
+
+	connection.release();
+});
